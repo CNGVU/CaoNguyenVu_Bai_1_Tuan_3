675228c0f7615941816e12adafe2c966863c9206
CNGVU <caonguyenvu191@gmail.com>
2023-09-25 21:23:29 +0700

    first commit

--- app/src/main/java/com/example/caonguyenvu_162_baitap/Register.java
+++ app/src/main/java/com/example/caonguyenvu_162_baitap/Register.java
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
-    TextView txt ;
+    private Button button ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        txt = findViewById(R.id.btnSIGNIN);
-//
+        button =(Button) findViewById(R.id.btnSIGNIN);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent my = new Intent(Register.this, Login.class);
+                startActivity(my);
+            }
+        });
     }
+
 }
